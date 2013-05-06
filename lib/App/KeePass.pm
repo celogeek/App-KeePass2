e87b0a902a05462ed0fc0ad1a9e6fd8f643c45d7
celogeek <me@celogeek.com>
2013-05-06 20:30:03 +0200

    init dist zilla

--- lib/App/KeePass.pm
+++ lib/App/KeePass.pm
@@ -1,10 +1,18 @@
 package App::KeePass;
+
+# ABSTRACT: KeePass commandline tools
+
+use strict;
+use warnings;
+# VERSION
 use Moo;
 use MooX::Options;
+use File::KeePass;
 
 sub run {
 	my ($self) = @_;
 
+	return;
 }
 
 1;
