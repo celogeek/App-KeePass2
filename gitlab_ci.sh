5f1f20a92f4fd78070c9a346ab17e9ddef31b04a
celogeek <me@celogeek.com>
2013-05-10 01:02:38 +0200

    add mirror cpan celogeek and verbose

--- gitlab_ci.sh
+++ gitlab_ci.sh
@@ -5,6 +5,7 @@ set -e
 source "$HOME/perl5/perlbrew/etc/bashrc"
 perlbrew use 5.16.3
 perlbrew install-cpanm
+export PERL_CPANM_OPT="--mirror http://cpan.celogeek.com -v"
 cpanm Dist::Zilla
 dzil authordeps --missing | cpanm
 dzil listdeps --missing | cpanm
