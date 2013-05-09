178f437559ef9d9219c35f8e4b0e5b04eb581f30
celogeek <me@celogeek.com>
2013-05-10 00:38:43 +0200

    add gitlab_ci builder

---
+++ gitlab_ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+perlbrew use 5.16.3
+cpanm Dist::Zilla
+dzil authordeps --missing | cpanm -v
+dzil listdeps --missing | cpanm -v
+dzil install --install-command='cpanm -v .'
