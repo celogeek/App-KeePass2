a5dba0201680c4121f11cb6d57635e8e97db86a8
celogeek <me@celogeek.com>
2013-05-10 01:36:50 +0200

    fix upload to github

--- gitlab_ci.sh
+++ gitlab_ci.sh
@@ -10,4 +10,9 @@ cpanm Dist::Zilla
 dzil authordeps --missing | cpanm
 dzil listdeps --missing | cpanm
 dzil cover
+dzil clean
+git checkout master
+git reset --hard origin/master
+git checkout devel
+git reset --hard origin/devel
 git push --mirror git@github.com:celogeek/App-KeePass2.git
