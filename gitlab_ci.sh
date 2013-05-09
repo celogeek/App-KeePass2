0b9056299f9aa656dc7bbbc0ffd40f3749aee17e
celogeek <me@celogeek.com>
2013-05-10 00:52:19 +0200

    add cpanm

--- gitlab_ci.sh
+++ gitlab_ci.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-set -x -e
+set -e
 
 source "$HOME/perl5/perlbrew/etc/bashrc"
 perlbrew use 5.16.3
+perlbrew install-cpanm
 cpanm Dist::Zilla
 dzil authordeps --missing | cpanm -v
 dzil listdeps --missing | cpanm -v
