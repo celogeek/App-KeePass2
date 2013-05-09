2b5cc9d3615522ec19569d42b2c001b54710956a
celogeek <me@celogeek.com>
2013-05-10 00:58:39 +0200

    improve test

--- gitlab_ci.sh
+++ gitlab_ci.sh
@@ -6,6 +6,6 @@ source "$HOME/perl5/perlbrew/etc/bashrc"
 perlbrew use 5.16.3
 perlbrew install-cpanm
 cpanm Dist::Zilla
-dzil authordeps --missing | cpanm -v
-dzil listdeps --missing | cpanm -v
-dzil install --install-command='cpanm -v .'
+dzil authordeps --missing | cpanm
+dzil listdeps --missing | cpanm
+dzil test
