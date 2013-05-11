c21b87ff06a0d2d2fca6f148c840beeb05987228
celogeek <me@celogeek.com>
2013-05-11 15:04:18 +0200

    tidy

--- lib/App/KeePass2.pm
+++ lib/App/KeePass2.pm
@@ -4,6 +4,7 @@ package App::KeePass2;
 
 use strict;
 use warnings;
+
 # VERSION
 use utf8::all;
 use Moo;
@@ -19,12 +20,13 @@ use Data::Printer;
 The password file
 
 =cut
+
 option 'file' => (
-	doc => 'Your keepass2 file',
-	is => 'ro',
-	short => 'f',
-	required => 1,
-	format => 's',
+    doc      => 'Your keepass2 file',
+    is       => 'ro',
+    short    => 'f',
+    required => 1,
+    format   => 's',
 );
 
 =attr create
@@ -32,10 +34,11 @@ option 'file' => (
 Create the keepass2 file
 
 =cut
+
 option 'create' => (
-	doc => 'Create a keepass2 file',
-	is => 'ro',
-	short => 'c',
+    doc   => 'Create a keepass2 file',
+    is    => 'ro',
+    short => 'c',
 );
 
 =attr dump_groups
@@ -43,17 +46,18 @@ option 'create' => (
 Dump the content of the groups
 
 =cut
+
 option 'dump_groups' => (
-	doc => 'Dump the groups of a keepass2 file',
-	is => 'ro',
-	short => 'd',
+    doc   => 'Dump the groups of a keepass2 file',
+    is    => 'ro',
+    short => 'd',
 );
 
 has _fkp => (
-	is => 'ro',
-	default => sub {
-		File::KeePass->new;
-	}
+    is      => 'ro',
+    default => sub {
+        File::KeePass->new;
+    }
 );
 
 =method run
@@ -65,44 +69,65 @@ Start the cli app
   $keepass->run;
 
 =cut
+
 sub run {
-	my ($self) = @_;
-	$self->_create, return if ($self->create);
-	$self->_dump_groups, return if ($self->dump_groups);
-	return;
+    my ($self) = @_;
+    $self->_create,      return if ( $self->create );
+    $self->_dump_groups, return if ( $self->dump_groups );
+    return;
 }
 
 sub _get_master_key {
-	my ($self) = @_;
-	return "" . prompt("Master Password : ", -e => "*", -tty);
+    my ($self) = @_;
+    return "" . prompt( "Master Password : ", -e => "*", -tty );
 }
 
 sub _get_confirm_key {
-	my ($self) = @_;
-	return "" . prompt("Confirm Password : ", -e => "*", -tty);
+    my ($self) = @_;
+    return "" . prompt( "Confirm Password : ", -e => "*", -tty );
 }
 
 sub _create {
-	my ($self) = @_;
-	croak "The file already exists !" if -f $self->file;
-	$self->_fkp->clear;
-	my $root = $self->_fkp->add_group({title => 'My Passwords', icon => $self->get_icon_id_from_key('key')});
-	my $gid = $root->{'id'};
-	$self->_fkp->add_group({title => 'Internet', group => $gid, icon => $self->get_icon_id_from_key('internet')});
-	$self->_fkp->add_group({title => 'Private', group => $gid, icon => $self->get_icon_id_from_key('key5')});
-	$self->_fkp->add_group({title => 'Bank', group => $gid, icon => $self->get_icon_id_from_key('dollar')});
-	$self->_fkp->unlock if $self->_fkp->is_locked;
-	my $master = $self->_get_master_key;
-	my $confirm = $self->_get_confirm_key;
-	croak "Your master password is different from the confirm password !" if $master ne $confirm;
-	$self->_fkp->save_db($self->file, $master);
-	return;
+    my ($self) = @_;
+    croak "The file already exists !" if -f $self->file;
+    $self->_fkp->clear;
+    my $root = $self->_fkp->add_group(
+        {   title => 'My Passwords',
+            icon  => $self->get_icon_id_from_key('key')
+        }
+    );
+    my $gid = $root->{'id'};
+    $self->_fkp->add_group(
+        {   title => 'Internet',
+            group => $gid,
+            icon  => $self->get_icon_id_from_key('internet')
+        }
+    );
+    $self->_fkp->add_group(
+        {   title => 'Private',
+            group => $gid,
+            icon  => $self->get_icon_id_from_key('key5')
+        }
+    );
+    $self->_fkp->add_group(
+        {   title => 'Bank',
+            group => $gid,
+            icon  => $self->get_icon_id_from_key('dollar')
+        }
+    );
+    $self->_fkp->unlock if $self->_fkp->is_locked;
+    my $master  = $self->_get_master_key;
+    my $confirm = $self->_get_confirm_key;
+    croak "Your master password is different from the confirm password !"
+        if $master ne $confirm;
+    $self->_fkp->save_db( $self->file, $master );
+    return;
 }
 
 sub _dump_groups {
-	my ($self) = @_;
-	$self->_fkp->load_db($self->file, $self->_get_master_key);
-	p($self->_fkp->groups);
-	return;
+    my ($self) = @_;
+    $self->_fkp->load_db( $self->file, $self->_get_master_key );
+    p( $self->_fkp->groups );
+    return;
 }
 1;
